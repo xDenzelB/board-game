358276e4b1cc87d034e0ef3d03f8fa4a6ff631c3
Denzel <dbartolaba123@gmail.com>
2022-02-09 16:27:21 -0800

    finished the list page

--- src/Game.js
+++ src/Game.js
@@ -3,10 +3,12 @@ import { Link } from 'react-router-dom';
 export default function Game({ game }) {
   return (
     // be sure this component is wrapped in a react-router link that takes the user to the correct detail page
-    <div className='game'>
-      <h3>{game.title}</h3>
-      <p>A {game.genre} game by designer {game.designer}</p>
-      <p>for {game.min_players} - {game.max_players} players</p>
-    </div>
+    <Link to={`/board-games/${game.id}`}>
+      <div className='game'>
+        <h3>{game.title}</h3>
+        <p>A {game.genre} game by designer {game.designer}</p>
+        <p>for {game.min_players} - {game.max_players} players</p>
+      </div>
+    </Link>
   );
 }
--- src/ListPage.js
+++ src/ListPage.js
@@ -4,10 +4,24 @@ import Game from './Game';
 
 export default function ListPage() {
   // you'll need some state to hold onto the array of games
+  const [games, setGames] = useState([]);
 
   // fetch the games on load and inject them into state
+
+  useEffect(() => {
+    async function fetch() {
+      const fetchGame = await getGames();
+
+      setGames(fetchGame);
+    }
+
+    fetch();
+  }, []);
   return (
     <div className='list games'>
+      {
+        games.map(game => <Game key={`${game.id}`} game={game} />)
+      }
       {/* map through the games in state and render Game components */}
     </div>
   );
